d0ceb89504fc09f0821cdeb78c30d63caebe03b6
MrDataPsycho <mr.data.psycho@gmail.com>
2024-05-25 20:41:06 +0200

    Adding new chat user and assistant into the chat

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -6,8 +6,48 @@
 # from chainlit.playground.providers import ChatOpenAI  # importing ChatOpenAI tools
 from dotenv import load_dotenv
 import streamlit as st
-# import asyncio
+import random
+import time
 
 load_dotenv()
 
-st.header("Welcome to the app!")
+st.title("Rabable: Your Dream Bot")
+
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
